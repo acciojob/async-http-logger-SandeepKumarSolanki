0d0444280f2c0a485dc20e55cf291198d0006d94
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-24 15:04:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+async function  fetchData(){
+	try {
+		let response = await fetch(`https://jsonplaceholder.typicode.com/todos/1`);
+		let data = await response.json();
+		console.log(data);
+	} catch (error) {
+		console.log("Error in fetching data :- ",error.message);
+	}
+}
+fetchData();
